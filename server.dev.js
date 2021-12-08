b56109689a2204f52da2dda211372d2f8d81b739
superrache@hotmail.fr <superrache@hotmail.fr>
2021-12-08 16:41:06 +0100

    fix: deploy ok but without vue-cli-express:watch

---
+++ server.dev.js
@@ -0,0 +1,16 @@
+/**
+ * The dev server (express only)
+ */
+
+var express = require('express')
+
+const app = express()
+
+var port = process.env.PORT || 3000
+
+app.listen(port)
+
+const api = require('./server/api.js')
+api(app, process.env.DATABASE_URL)
+
+console.log('Server listening on: '+ port)
