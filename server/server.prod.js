6016e7316349ee8a445bfb97d66f68b93a3fd9a1
superrache@hotmail.fr <superrache@hotmail.fr>
2021-12-13 23:10:03 +0100

    fix: remove serve-static

--- server/server.prod.js
+++ server/server.prod.js
@@ -9,7 +9,7 @@ const app = express()
 
 const staticDir = __dirname + "/../dist"
 console.log('Serving static files: ' + staticDir)
-app.use(serveStatic(staticDir))
+app.use(express.static(staticDir))
 
 var port = process.env.PORT || 3001
 app.listen(port)
