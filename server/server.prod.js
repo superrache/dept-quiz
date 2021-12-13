31cfa416d8b10be43ca10bef7597ea281134d554
superrache@hotmail.fr <superrache@hotmail.fr>
2021-12-13 16:33:27 +0100

    fix: prod static dir

--- server/server.prod.js
+++ server/server.prod.js
@@ -7,9 +7,11 @@ var serveStatic = require('serve-static')
 
 const app = express()
 
-app.use(serveStatic(__dirname + "../dist"))
-var port = process.env.PORT || 3001
+const staticDir = __dirname + "/../dist"
+console.log('Serving static files: ' + staticDir)
+app.use(serveStatic(staticDir))
 
+var port = process.env.PORT || 3001
 app.listen(port)
 
 const api = require('./api.js')
